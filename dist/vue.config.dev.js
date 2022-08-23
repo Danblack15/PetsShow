ddf78579878c08f658f35c45466a41915819f716
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-08-23 16:10:56 +0500

    General

---
+++ dist/vue.config.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _require = require('@vue/cli-service'),
+    defineConfig = _require.defineConfig;
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/Pets/'
+});
